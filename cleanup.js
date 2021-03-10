00f092e996d511bc1c982df14d4afc4072d0d12f
Benjamin Goldsbury <bpgoldsb@bpgoldsb.com>
2021-03-10 11:56:29 -0800

    Add description for multiple windows on cleanup

--- cleanup.js
+++ cleanup.js
@@ -22,12 +22,7 @@ function setupCloseButton(totalTabs) {
 
 function createListItem(tabs, tabWindowCount) {
     // Add information about duplicate tabs/files
-    // let liNode = document.createElement("LI")
     let docTitle = tabs[0].title
-    // let item = createLineItemText(docTitle, tabs, tabWindowCount)
-    // liNode.appendChild(item)
-
-
     let newNode = document.querySelector('.list-template').cloneNode(true);
 
     // Remove class reserved for template node
@@ -37,29 +32,17 @@ function createListItem(tabs, tabWindowCount) {
 
     // Set count variable for .cleanUpDataCount
     newNode.children[1].textContent = tabs.length + 1
+
+    // Populate window count or remove text if only in a single window
+    if (tabWindowCount > 1) {
+        newNode.children[2].children[0].textContent = tabWindowCount
+    } else {
+        newNode.children[2].remove()
+    }
+    
     document.getElementById(cleanupDataDOMId).appendChild(newNode)
 }
 
-function createLineItemText(docTitle, tabs, tabWindowCount) {
-    // Create the text node that contains the information about 1 closeable group of tabs
-    let textParts = []
-    let listItem = document.createElement('p')
-    textParts.push(document.createTextNode("Google Drive file "))
-    let listItemPart2 = document.createElement("u")
-    let listItemPart2a = document.createElement("b")
-    let listItemPart2b = document.createTextNode(docTitle)
-    listItemPart2.appendChild(listItemPart2a)
-    listItemPart2a.appendChild(listItemPart2b)
-    textParts.push(listItemPart2)
-    textParts.push(document.createTextNode(` is open in ${tabs.length + 1} tabs`))
-
-    if (tabWindowCount > 1) {
-        textParts.push(document.createTextNode(` (across ${tabWindowCount} browser windows)`))
-    }
-    _.map(textParts, (p) => { listItem.appendChild(p) })
-
-    return listItem
-}
 
 let port = browser.runtime.connect({name: 'ntlb-cleanup'})
 port.onMessage.addListener(onMessage)
